3fcc347c39d41257addd43864ed6171f3974c9ce
Sabarinath.M <36480817+Jack-sparrow07@users.noreply.github.com>
2018-02-16 17:39:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/article-one', function (req, res) {
+    res.send("article one is serving");
+});
+
+app.get('/article-two', function (req, res) {
+    res.send("article two is serving");
+});
+
+app.get('/article-three', function (req, res) {
+    res.send("article three is serving");
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
