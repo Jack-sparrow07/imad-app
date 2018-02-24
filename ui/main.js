435030d7fa4a8659f00bdc9ffdd1b9bff01d7751
Sabarinath.M <36480817+Jack-sparrow07@users.noreply.github.com>
2018-02-24 10:40:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element = document.getElementById('new-value');
 element.innerHTML = "new value";
 var img = document.getElementById('madi');
 var marginLeft =0;
-function marginRight(){
+function moveRight(){
     marginLeft=marginLeft + 10;
   img.style.marginLeft =marginLeft + 'px';
 }
