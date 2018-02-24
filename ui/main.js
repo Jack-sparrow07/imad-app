b713b7c009f11871327c52fe12da9a8d26115aeb
Sabarinath.M <36480817+Jack-sparrow07@users.noreply.github.com>
2018-02-24 10:18:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('new-value');
 element.innerHTML = "new value";
-var img = document.getElementById('madi');
+/*var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginleft = '100px';  
-};
+};*/
